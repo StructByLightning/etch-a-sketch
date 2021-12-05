e3d2a93c0efeb3e6773d8968c6a65059aaa147e4
StructByLightning <mossygreenrock@gmail.com>
2021-12-04 18:30:44 -0800

    linting

--- components/etch-a-sketch-canvas/index.jsx
+++ components/etch-a-sketch-canvas/index.jsx
@@ -3,7 +3,7 @@ import RangeSlider from "/components/range-slider/index.jsx";
 import css from "./index.module.scss";
 
 const CANVAS_SIZE = 1000;
-function randint(max){
+function randint(max) {
   return Math.floor(Math.random() * max);
 }
 
@@ -17,46 +17,46 @@ export default function EtchASketch() {
 
   let canvas = useRef();
 
-  let style = {"--size": `${size}`};
+  let style = { "--size": `${size}` };
 
   function draw(event) {
-    const ctx = event.target.getContext('2d');
+    const ctx = event.target.getContext("2d");
     const rect = event.target.getBoundingClientRect();
 
-    //convert global coords into relative 
-    const oldX = Math.floor((pos[0] - rect.left)/(rect.width/CANVAS_SIZE));
-    const oldY = Math.floor((pos[1] - rect.top)/(rect.width/CANVAS_SIZE));
-    const newX = Math.floor((event.clientX - rect.left)/(rect.width/CANVAS_SIZE));
-    const newY = Math.floor((event.clientY - rect.top)/(rect.width/CANVAS_SIZE));
+    //convert global coords into relative
+    const oldX = Math.floor((pos[0] - rect.left) / (rect.width / CANVAS_SIZE));
+    const oldY = Math.floor((pos[1] - rect.top) / (rect.width / CANVAS_SIZE));
+    const newX = Math.floor((event.clientX - rect.left) / (rect.width / CANVAS_SIZE));
+    const newY = Math.floor((event.clientY - rect.top) / (rect.width / CANVAS_SIZE));
 
-    ctx.beginPath(); 
-  
+    ctx.beginPath();
+
     ctx.lineWidth = 10;
-    ctx.lineCap = 'round';
-    ctx.strokeStyle = "hsl("+hue+",50%, 50%)";
-    if (rainbow){
-      let h = hue+3;
-      if (h > 360){
+    ctx.lineCap = "round";
+    ctx.strokeStyle = `hsl(${hue},50%, 50%)`;
+    if (rainbow) {
+      let h = hue + 3;
+      if (h > 360) {
         h = 0;
       }
-      setHue(h)
+      setHue(h);
     }
-  
-    ctx.moveTo(oldX, oldY); 
+
+    ctx.moveTo(oldX, oldY);
     setPos([event.clientX, event.clientY]);
-    ctx.lineTo(newX, newY); 
-    ctx.stroke(); 
+    ctx.lineTo(newX, newY);
+    ctx.stroke();
   }
 
   return (
     <div className={css.etchASketch}>
-      <canvas 
-        ref={canvas} 
-        style={style} 
-        onMouseMove={(event)=>draw(event)} 
-        onMouseDown={(event)=>setPos([event.clientX, event.clientY])} 
-        onMouseEnter={(event)=>setPos([event.clientX, event.clientY])} 
-        width={CANVAS_SIZE} 
+      <canvas
+        ref={canvas}
+        style={style}
+        onMouseMove={(event) => draw(event)}
+        onMouseDown={(event) => setPos([event.clientX, event.clientY])}
+        onMouseEnter={(event) => setPos([event.clientX, event.clientY])}
+        width={CANVAS_SIZE}
         height={CANVAS_SIZE}
       />
       <div className={css.controls}>

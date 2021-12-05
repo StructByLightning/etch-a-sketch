86e2cb27a79cd4b2b4d6f1f64554c176feaeb33b
StructByLightning <mossygreenrock@gmail.com>
2021-12-04 16:34:34 -0800

    boilerplate. mix of create-next-app and stuff I use in every project like core.scss and _variables.scss

--- next.config.js
+++ next.config.js
@@ -1,3 +1,14 @@
 module.exports = {
   reactStrictMode: true,
-}
+  experimental: { optimizeCss: true },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      loader: "@svgr/webpack",
+      options: {
+        svgo: false,
+      },
+    });
+    return config;
+  },
+};
